fd3d9e0aeb44de183a694cf798e859f8f6ffd9cc
Hazer <3169443421@qq.com>
2025-04-11 07:12:19 +0000

    2025/4/11-lcz-nothing

--- main.cpp
+++ main.cpp
@@ -5,11 +5,8 @@ using namespace rpc1k;
 
 int main() {
     Real r1("4.316944341002E04"), r2("5.768091431E5"), r3;
-    kernel_add_with_carry(
-        reinterpret_cast<const BasicNum*>(r1.p.get()),
-        reinterpret_cast<const BasicNum*>(r2.p.get()),
-        reinterpret_cast<BasicNum*>(r3.p.get())
-    );
-    std::cout << r1 << std::endl << r2 << std::endl << r3 << std::endl;
+    std::cout << r1 << std::endl;
+    std::cout << r2 << std::endl;
+    std::cout << r3 << std::endl;
     return 0;
 }
