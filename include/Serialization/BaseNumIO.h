0e2203a00f726b82f580b4a1f26927f7640d61cc
Hazer <3169443421@qq.com>
2025-04-25 15:41:50 +0800

    2025/4/25-lcz-comment

--- include/Serialization/BaseNumIO.h
+++ include/Serialization/BaseNumIO.h
@@ -29,6 +29,47 @@ public:
 
 enum class io {binary, csv};
 
+/**
+ * @brief Parses floating-point numbers into fixed-point numbers with storage, output, and serialization capabilities.
+ *
+ * The RealParser class converts floating-point numbers (including scientific notation) into fixed-point numbers
+ * stored in BaseNum format (see: RPC-1K\include\Core\BaseNum.h). It supports:
+ * - Parsing from strings (including scientific notation like "1.23e-4")
+ * - Output as fixed-point string (std::string)
+ * - Serialization/deserialization in both CSV and binary formats
+ * 
+ * Usage Examples:
+ * @code
+ *    RealParser parser;
+ *    std::shared_ptr<BaseNum> num(std::make_shared<BaseNum>());
+ *    std::ofstream file_out("serialization_test.csv");
+ *
+ *    parser(num, "123451234.3e-1");       // Parse from string
+ *    parser(num, file_out);               // Serialize to CSV
+ *    file_out.close();
+ *    
+ *    std::shared_ptr<BaseNum> num2(std::make_shared<BaseNum>());
+ *    std::ifstream file_in("serialization_test.csv");
+ *    
+ *    parser(num2, file_in);               // Deserialize from CSV
+ *    std::cout << "Read csv: " << parser(num2) << std::endl;
+ *    file_in.close();
+ *    
+ *    file_out.open("serialization_test.bin");
+ *    parser(num, "-2344424.123424E-3");  // Parse another value
+ *    parser(num, file_out, ::rpc1k::io::binary);  // Serialize to binary
+ *    file_out.close();
+ *    
+ *    file_in.open("serialization_test.bin");
+ *    parser(num2, file_in, ::rpc1k::io::binary);  // Deserialize from binary
+ *    std::cout << "Read bin: " << parser(num2) << std::endl;
+ * @endcode
+ * 
+ * @note The class throws NumberFormatError for invalid input formats
+ * @author Hazer
+ * @date 2025/4/25
+ */
+
 class RealParser {
 private:
     std::shared_ptr<BaseNum> dst;
