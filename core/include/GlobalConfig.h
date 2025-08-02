1663264b41fb287dcdc47d2d1fef05ad8e0f03b1
18066118248 <22301092@bjtu.edu.cn>
2025-08-02 20:09:16 +0800

    2025/8/2-lcz-config

--- core/include/GlobalConfig.h
+++ core/include/GlobalConfig.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <regex>
 #include <variant>
 #include <iostream>
 #include <string_view>
@@ -9,25 +10,95 @@
 
 namespace mpengine {
 
+class GlobalConfig;
+
 class ConfigType {
-    private:
-        std::variant<int64_t, double, bool, std::string> data;
-    public:
-        ConfigType();
-        ~ConfigType();
-        template<typename Type>
-        ConfigType(Type&& value): data(std::forward<Type>(value)) {}
-        std::string_view get_type() const noexcept;
-        template<typename Type>
-        Type get_or_else(Type default_value) const noexcept {
-            try {
-                return std::get<Type>(data);
-            } catch(const std::bad_variant_access& e) {
-                return default_value;
-            }
+private:
+    std::variant<int64_t, double, bool, std::string> data;
+    void convert(const std::string& str) noexcept;
+    friend GlobalConfig;
+public:
+    ConfigType();
+    ~ConfigType();
+    ConfigType(const ConfigType&) = default;
+    ConfigType& operator = (const ConfigType&) = default;
+    ConfigType(ConfigType&&) = default;
+    ConfigType& operator = (ConfigType&&) = default;
+    template<typename Type>
+    ConfigType(Type&& value): data(std::forward<Type>(value)) {}
+    std::string_view get_type() const noexcept;
+    template<typename Type>
+    Type get_or_else(Type default_value) const noexcept {
+        try {
+            return std::get<Type>(data);
+        } catch(const std::bad_variant_access& e) {
+            return default_value;
         }
-        friend std::istream& operator >> (std::istream& stream, ConfigType& config) noexcept;
-        friend std::ostream& operator << (std::ostream& stream, const ConfigType& config) noexcept;
+    }
+    friend std::istream& operator >> (std::istream& stream, ConfigType& config) noexcept;
+    friend std::ostream& operator << (std::ostream& stream, const ConfigType& config) noexcept;
+};
+
+class ConfigNode {
+public:
+    using NodePtr = std::shared_ptr<ConfigNode>;
+public:
+    ConfigNode() = default;
+    ~ConfigNode() = default;
+};
+
+class ConfigDomainNode: public ConfigNode {
+public:
+    using NodePtr = std::shared_ptr<ConfigNode>;
+private:
+    std::map<std::string, NodePtr> children;
+    friend GlobalConfig;
+public:
+    ConfigDomainNode();
+    ~ConfigDomainNode();
+    ConfigDomainNode(const ConfigDomainNode&) = delete;
+    ConfigDomainNode& operator = (const ConfigDomainNode&) = delete;
+    ConfigDomainNode(ConfigDomainNode&&) = delete;
+    ConfigDomainNode& operator = (ConfigDomainNode&&) = delete;
+};
+
+class ConfigValueNode: public ConfigNode {
+public:
+    using NodePtr = std::shared_ptr<ConfigNode>;
+private:
+    ConfigType value;
+    friend GlobalConfig;
+public:
+    ConfigValueNode(const ConfigType& value);
+    ~ConfigValueNode();
+    ConfigValueNode(const ConfigValueNode&) = delete;
+    ConfigValueNode& operator = (const ConfigValueNode&) = delete;
+    ConfigValueNode(ConfigValueNode&&) = delete;
+    ConfigValueNode& operator = (ConfigValueNode&&) = delete;
+};
+
+class GlobalConfig {
+public:
+    using NodePtr = std::shared_ptr<ConfigNode>;
+private:
+    static const std::regex valid_key;
+    static std::string config_filepath;
+    static size_t indent;
+    static std::atomic<bool> initialized;
+    static std::mutex setting_lock;
+    NodePtr root;
+    GlobalConfig();
+    ~GlobalConfig();
+public:
+    GlobalConfig(const GlobalConfig&) = delete;
+    GlobalConfig& operator = (const GlobalConfig&) = delete;
+    GlobalConfig(GlobalConfig&&) = delete;
+    GlobalConfig& operator = (GlobalConfig&&) = delete;
+    static bool set_global_config(
+        const std::string& config_filepath = GlobalConfig::config_filepath,
+        size_t indent = GlobalConfig::indent
+    ) noexcept;
+    static GlobalConfig& get_global_config() noexcept;
 };
 
 }
--- core/src/GlobalConfig.cpp
+++ core/src/GlobalConfig.cpp
@@ -2,9 +2,9 @@
 
 namespace mpengine {
 
-ConfigType::ConfigType(): data(false) {};
+ConfigType::ConfigType(): data(false) {}
 
-ConfigType::~ConfigType() {};
+ConfigType::~ConfigType() {}
 
 std::string_view ConfigType::get_type() const noexcept {
     if (std::holds_alternative<int64_t>(data)) {
@@ -19,33 +19,38 @@ std::string_view ConfigType::get_type() const noexcept {
     return "unknown";
 }
 
-std::istream& operator >> (std::istream& stream, ConfigType& config) noexcept {
-    std::string str;
-    stream >> str;
+void ConfigType::convert(const std::string& str) noexcept {
     size_t pos;
     try {
         int64_t value = std::stoll(str, &pos);
         if (pos == str.length()) {
-            config.data = value;
-            return stream;
+            data = value;
+            return;
         }
     } catch(...) {}
     try {
         double value = std::stod(str, &pos);
         if (pos == str.length()) {
-            config.data = value;
-            return stream;
+            data = value;
+            return;
         }
     } catch(...) {}
     if (str == "true" || str == "True") {
-        config.data = true;
-        return stream;
+        data = true;
+        return;
     }
     if (str == "false" || str == "False") {
-        config.data = false;
-        return stream;
+        data = false;
+        return;
     }
-    config.data = str;
+    data = str;
+    return;
+}
+
+std::istream& operator >> (std::istream& stream, ConfigType& config) noexcept {
+    std::string str;
+    stream >> str;
+    config.convert(str);
     return stream;
 }
 
@@ -56,6 +61,44 @@ std::ostream& operator << (std::ostream& stream, const ConfigType& config) noexc
         std::visit([&stream](auto&& arg) { stream << arg; }, config.data);
     }
     return stream;
+}
+
+ConfigDomainNode::ConfigDomainNode(): children() {}
+
+ConfigDomainNode::~ConfigDomainNode() {}
+
+ConfigValueNode::ConfigValueNode(const ConfigType& value): value(value) {}
+
+ConfigValueNode::~ConfigValueNode() {}
+
+const std::regex GlobalConfig::valid_key("[a-zA-Z0-9_.]+(/[a-zA-Z0-9_.]+)*");
+std::string GlobalConfig::config_filepath("configurations.conf");
+size_t GlobalConfig::indent = 4;
+std::atomic<bool> GlobalConfig::initialized = false;
+std::mutex GlobalConfig::setting_lock;
+
+GlobalConfig::GlobalConfig() {
+    std::lock_guard<std::mutex> lock(GlobalConfig::setting_lock);
+    GlobalConfig::initialized.store(true, std::memory_order_release);
+}
+
+GlobalConfig::~GlobalConfig() {}
+
+bool GlobalConfig::set_global_config(const std::string& config_filepath, size_t indent) noexcept {
+    auto& logger = putils::RuntimeLog::get_global_log();
+    std::lock_guard<std::mutex> lock(GlobalConfig::setting_lock);
+    if (indent == 0) {
+        logger.add("(Configuration): All numeric arguments must be positive integers.");
+        return false;
+    }
+    GlobalConfig::config_filepath = config_filepath;
+    GlobalConfig::indent = indent;
+    return true;
+}
+
+GlobalConfig& GlobalConfig::get_global_config() noexcept {
+    static GlobalConfig config_instance;
+    return config_instance;
 }
 
 }
