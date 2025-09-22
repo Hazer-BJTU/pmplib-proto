d4e26e03e8611d95ac5a16e9e3d24ce9e4187763
18066118248 <22301092@bjtu.edu.cn>
2025-09-22 16:18:48 +0800

    2025/9/22-lcz

--- core/test/integertest.cpp
+++ core/test/integertest.cpp
@@ -1,20 +1,10 @@
 #include <vector>
+#include <iomanip>
+
 #include "pmp/integer.h"
+#include "MemoryAllocator.h" 
 
 int main() {
-    pmp::context context(1000, pmp::io::hex);
-    std::vector<pmp::integer> arr;
-    pmp::integer a("0", context), b("1", context);
-    arr.push_back(a);
-    arr.push_back(b);
-    for (size_t i = 2; i < 10; i++) {
-        arr.push_back(arr[i - 1] + arr[i - 2]);
-    }
-    pmp::integer c = arr[9], d = arr[6];
-    arr.clear();
-    context.generate_procedures();
-    context.await_pipeline_accomplish();
-    context.clean_up();
-    std::cout << c << std::endl;
+    
     return 0;
 }
